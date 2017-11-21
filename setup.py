c6391d5345a90b70cb19b015293cd16833b00e91
igor <jovic.i24@gmail.com>
2017-11-21 23:46:05 +0100

    add setup configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datapreprocessor',
+      version='0.1',
+      description='A tool for preprocessing data for data analasys and machine learning.',
+      url='https://github.com/jovicigor/DataPreprocessor',
+      author='Igor Jovic',
+      author_email='jovic.i24@gmail.com',
+      license='MIT',
+      packages=['datapreprocessor'],
+      zip_safe=False)
