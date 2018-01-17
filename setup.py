2f29724c1a0e53cad9b32eb41b13260a191ba83f
igor <jovic.i24@gmail.com>
2018-01-17 14:27:02 +0100

    fix version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='datapreprocessor',
-      version='0.2',
+      version='0.2.1',
       description='A tool for preprocessing data for data analasys and machine learning.',
       url='https://github.com/jovicigor/DataPreprocessor',
       author='Igor Jovic',
